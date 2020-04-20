ec0e8d20317c4ebdca7cee424a83aacfad3897f0
Jerry Shockley <noise1@shockleynet.com>
2020-04-20 18:02:36 -0400

    Added a host variable for matching the private network.

--- app/app.js
+++ app/app.js
@@ -5,10 +5,11 @@
 // Load the express module.
 const express = require('express'),
     app = express(),
-    port = 8080
+    port = 80
+    host = '192.168.5.89'
 
 // Respond to requests for / with 'Hello World'.
 app.get('/', (req, res) => res.send('Hello Jerry!'))
 
 // Listen on port 80 (like a true web server).
-app.listen(port, () => console.log('Express server started successfully.'))
+app.listen(port, host, () => console.log('Express server started successfully.'))
