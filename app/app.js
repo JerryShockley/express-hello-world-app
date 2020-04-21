529d3f33caf2e13a469ff945e3e726fa298b79c1
Jerry Shockley <noise1@shockleynet.com>
2020-04-21 09:01:00 -0400

    Enabled passing in of host and port values via command line variables.
    Created default host value of '127.0.0.1' and port value
    of '8080'. Values passed in from the command line in the form
    of key=value where key is host or port will override the
    default values.
    
    This functionality enables the configuration of the app without
    having to checkout the code, modify the hard coded values, and
    check the changes back in somewhere.

--- app/app.js
+++ app/app.js
@@ -5,11 +5,28 @@
 // Load the express module.
 const express = require('express'),
     app = express(),
-    port = 80
-    host = '192.168.5.89'
+    port = parseArgv('port', process.argv) || 8080
+    host = parseArgv('host', process.argv) || '127.0.0.1'
+
+console.log(`Express is listening on '${host}:${port}'`)
 
 // Respond to requests for / with 'Hello World'.
-app.get('/', (req, res) => res.send('Hello Jerry!'))
+app.get('/', (req, res) => res.send('Hello from Node and Express!'))
 
-// Listen on port 80 (like a true web server).
+// Listen on port 8080 (like a true web server).
 app.listen(port, host, () => console.log('Express server started successfully.'))
+
+// Simple command line arg parser that
+// searches for a specified key where
+// the form of the CLI argument is key=value.
+// The function returns the value associated
+// with the key.
+function parseArgv(key, argv) {
+  idx = argv.indexOf(key, 2)
+  if( idx == -1) {
+    result = false // We didn't find the key
+  } else {  // We found our key
+    result = argv[idx].split('=').trim() // extract key value
+  }
+  return result
+}
